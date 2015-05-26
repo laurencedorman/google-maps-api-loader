0391a18063d9bcc47dcbba507a619033eb018855
Laurence Dorman <ldorman@letudiant.fr>
2015-05-26 11:13:51 +0200

    attempting to implement coverage

--- karma.conf.js
+++ karma.conf.js
@@ -27,6 +27,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'src/**.js': [ 'browserify', 'coverage' ],
       'test/**.js': [ 'browserify' ]
     },
 
@@ -34,8 +35,12 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['mocha'],
+    reporters: ['mocha', 'coverage'],
 
+    coverageReporter: {
+      type : 'html',
+      dir : 'coverage/'
+    },
 
     // web server port
     port: 9876,
