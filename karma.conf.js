b2da1e952e11f01d74e11e56e3e63cba8b16e3ef
Laurence Dorman <ldorman@letudiant.fr>
2015-05-26 10:55:07 +0200

    karma phantomjs

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'browserify'],
+    frameworks: ['mocha', 'browserify', 'phantomjs-shim'],
 
 
     // list of files / patterns to load in the browser
@@ -56,7 +56,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
 
 
     // Continuous Integration mode
